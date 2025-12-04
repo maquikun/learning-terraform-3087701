438506b4cbae5f6804696dcd44d131d34fd7c9af
Dylan Perotti <imrenderman@gmail.com>
2025-12-04 03:36:36 -0500

    Remove alb target

--- main.tf
+++ main.tf
@@ -54,16 +54,6 @@ module "blog-alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
-
   listeners = {
     ex-http = {
       port     = 80
